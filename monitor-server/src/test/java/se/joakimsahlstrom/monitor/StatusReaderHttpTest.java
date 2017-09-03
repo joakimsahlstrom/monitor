43e67f86afaeea19424aec8a4cbe8670c892990d
Joakim Sahlström <joakim.sahlstrom@gmail.com>
2017-09-03 10:46:36 +0200

    Lets not use the internet during normal unit test case executions

--- monitor-server/src/test/java/se/joakimsahlstrom/monitor/StatusReaderHttpTest.java
+++ monitor-server/src/test/java/se/joakimsahlstrom/monitor/StatusReaderHttpTest.java
@@ -1,5 +1,6 @@
 package se.joakimsahlstrom.monitor;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import se.joakimsahlstrom.monitor.model.Status;
 
@@ -11,6 +12,7 @@ public class StatusReaderHttpTest {
     private final StatusReader statusReader = new StatusReaderHttp();
 
     @Test
+    @Ignore
     public void canTestUrl() throws Exception {
         assertEquals(Status.OK, statusReader.getStatus(new URL("http://www.google.com")));
         assertEquals(Status.FAIL, statusReader.getStatus(new URL("http://www.this_is_not_a_real_address_329649238476298472389.com")));
